94e6dbf08b29e13eeaa4ab748126d06ca98a9cb7
sravan-kumar-ta <sravan0095@gmail.com>
2021-12-03 20:54:31 +0530

    create model and first view for display links

--- api/views.py
+++ api/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 
 # Create your views here.
-def home(request):
-    return HttpResponse('new project')
+@api_view(['GET'])
+def apiOverview(request):
+    api_urls = {
+        'List': '/task-list/',
+        'Detail View': '/task-detail/<str:pk>/',
+        'Create': '/task-create/',
+        'Update': '/task-update/<str:pk>/',
+        'Delete': '/task-delete/<str:pk>/',
+    }
+
+    return Response(api_urls)
